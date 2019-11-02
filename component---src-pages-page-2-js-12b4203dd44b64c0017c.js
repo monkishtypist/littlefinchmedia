48abcbb9d09857f1a615832b6a4b628b8b3931e4
Tim Spinks <tim@monkishtypist.com>
2019-11-02 09:31:56 -0700

    Updates

---
+++ component---src-pages-page-2-js-12b4203dd44b64c0017c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{241:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(169),c=a(239),r=a(238);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-12b4203dd44b64c0017c.js.map
---
+++ styles-9b3b525015f37909434e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{203:function(n,w,o){}}]);
+//# sourceMappingURL=styles-9b3b525015f37909434e.js.map
