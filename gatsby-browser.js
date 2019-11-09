78a6f1dd624001531932ee305f01aff69a7aeb1c
Tim Spinks <tim@monkishtypist.com>
2019-11-08 22:09:54 -0800

    prepend env vars with GATSBY_

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,9 +6,9 @@ import { AuthProvider } from "react-use-auth"
 // import "./src/styles/reset.css"
 
 const params = {
-  // domain: process.env.AUTH0_DOMAIN,
-  // clientID: process.env.AUTH0_CLIENTID,
-  redirectUri: process.env.AUTH0_CALLBACK,
+  // domain: process.env.GATSBY_AUTH0_DOMAIN,
+  // clientID: process.env.GATSBY_AUTH0_CLIENTID,
+  redirectUri: process.env.GATSBY_AUTH0_CALLBACK,
   // audience: `https://${auth0_domain}/api/v2/`,
   // responseType: "token id_token",
   // scope: "openid profile email"
@@ -17,8 +17,8 @@ const params = {
 export const wrapRootElement = ({ element }) => (
     <AuthProvider
         navigate={navigate}
-        auth0_domain={process.env.AUTH0_DOMAIN}
-        auth0_client_id={process.env.AUTH0_CLIENTID}
+        auth0_domain={process.env.GATSBY_AUTH0_DOMAIN}
+        auth0_client_id={process.env.GATSBY_AUTH0_CLIENTID}
         auth0_params={params}
     >
         {element}
